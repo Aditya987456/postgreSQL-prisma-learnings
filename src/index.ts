2c776d6453c47ecd5b94226c1dbc96b28587f809
Aditya raj <adityaraj987456@gmail.com>
2025-09-17 22:50:21 +0530

    writing sql to remove sql injection vulnerabilities

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Client } from "pg";    //Client is a class
 import express from "express"
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 const app = express()
+const db_url=process.env.DB_URL
+// console.log('db_url : '+db_url)
+// console.log('DB_URL :'+ process.env.DB_URL)
 app.use(express.json())
 
 //## method-1 :  initialize instances of postgress Client that let me do crud.
-const pgClient = new Client("postgresql://neondb_owner:npg_axDdNT42jMVu@ep-shiny-morning-a80g4onn-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require")
+const pgClient = new Client(db_url)
 
 
 //## method-2 :  const pgClient2 = new Client({
@@ -34,25 +40,43 @@ main()
 
 app.post('/signup', async (req, res)=>{
     const {username, email, password }=req.body
+    let insertQuery;
 
     try {
 
-     // -----**** this way of writing sql is the reason of sql injection hacker easily apply sql injection here.   
-        const insertQuery = `INSERT INTO users (username, email, password) VALUES (
-        '${username}', '${email}', '${password}'); `
-        const response = await pgClient.query(insertQuery)
+    // -----**** this way of writing sql is the reason of sql injection hacker easily apply sql injection here.   
+        // insertQuery = `INSERT INTO users (username, email, password) VALUES (
+        // '${username}', '${email}', '${password}'); `
+        // const response = await pgClient.query(insertQuery)
+
+    //----------------------this is the sql injection---> 
+        // " 123456'); DELETE FROM users; INSERT INTO users (username, email, password) VALUES ('hacked' 'hacked@gmail.com' 'hacked123 " 
+
+    //updated code that remove sql injection vulnerabilities...
+        insertQuery=`INSERT INTO users (username, email, password) VALUES ($1, $2, $3)` //i.e value -1,2,3
+
+        const response = await pgClient.query(insertQuery, [username, email, password])
+        console.log('Response ---> '+response)
+
+
+
 
         console.log(response.rows)
+        console.log('insertQuery : '+ insertQuery)
         res.status(200).json({
             message:"Successfully signed up"
         })
         
     } catch (error) {
         console.log('Error in signing in --> '+ error)
-        res.status(401).json({
-            message :"Error in signing up"
+        res.status(403).json({
+            message :"Error in signing up",
+            Error:error
         })
     }
+
+    console.log(insertQuery)
+
 
 
 })
