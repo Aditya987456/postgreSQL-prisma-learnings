49976ab78f97820fbc8097a524011ee37c243d1b
Aditya raj <adityaraj987456@gmail.com>
2025-11-08 23:47:35 +0530

    added relation in prisma

--- prisma-learning/dist/index.js
+++ prisma-learning/dist/index.js
@@ -1,4 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log('prisma project initialize.');
+//-- importing prismaClient becz that is responsible for converting my simple syntax query into raw SQL queries.
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient(); //instance of the class.
+async function CreateUser() {
+    client.user.create({
+        data: {
+            username: "buttan sarkar",
+            password: "123456",
+            age: "21",
+            city: "Prayagraj"
+        }
+    });
+}
+CreateUser();
 //# sourceMappingURL=index.js.map
--- prisma-learning/src/index.ts
+++ prisma-learning/src/index.ts
@@ -1,1 +1,62 @@
-console.log('prisma project initialize.')
+
+//-- importing prismaClient becz that is responsible for converting my simple syntax query into raw SQL queries.
+
+import { PrismaClient } from "@prisma/client"
+
+const client = new PrismaClient();   //instance of the class.
+
+
+async function CreateUser() {
+
+//----create user------
+
+//     await client.user.create({
+//     data:{
+//         username:"buttan sarkar",
+//         password:"123456",
+//         age:"21",
+//         city:"Prayagraj"
+//     }
+// })
+
+//-----delete user -----
+
+//     await client.user.delete({
+//     where: {
+//         id:1
+//     }
+// })
+
+
+//-----update user -----
+
+//     await client.user.update({
+//     where: {
+//         id:2
+//     }, 
+//     data:{
+//         username:'aman sarkar'
+//     }
+// })
+
+//---read the data---
+
+   const userData= await client.user.findFirst({
+    where: {
+        id:2
+    },
+    // select:{
+    //     username:true   //only need user name.
+    // }
+})
+
+
+console.log('data from db ----- ', userData?.password)
+
+
+    
+}
+
+CreateUser();
+
+
